086dfba33fc68187bae72a2c5eb2887df05741dc
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2023-11-12 16:35:56 +0700

    app layout

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-[#4E9F3D] focus:ring-[#4E9F3D] rounded-md shadow-sm']) !!}>
