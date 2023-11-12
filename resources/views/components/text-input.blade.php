3b043454cf84477e39b3b90de3e94ee091f3a140
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2023-11-12 20:25:06 +0700

    landing page starter

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-[#4E9F3D] focus:ring-[#4E9F3D] rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-[#4E9F3D] focus:ring-[#4E9F3D] rounded-md shadow-sm focus:text-black']) !!}>
